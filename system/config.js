2e4fe5dbed20dc65eedd1aa34a888e80126daab2
alexpaco15 <100037748+alexpaco15@users.noreply.github.com>
2022-08-20 20:18:34 +0200

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,5 +1,5 @@
 // Owner number
-global.owner = '6285221100126'
+global.owner = '34612538080'
 // Owner name
 global.owner_name = 'Wildan Izzudin'
 // Maximum upload file size limit (Default : 250 MB)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6285221100126@c.us'
+global.forwards = '34612538080@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))('5VC9rvNx')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
